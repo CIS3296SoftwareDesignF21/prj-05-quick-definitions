8d5f24b35fce15084e3e8188fb2ab7e068f7d72a
tuh26410 <70284991+tuh26410@users.noreply.github.com>
2021-11-18 12:10:17 -0500

    Removed logging from updateSelected. Added divs to popup.html

--- updateSelected.js
+++ updateSelected.js
@@ -6,17 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // The handler also must go in a .js file
 function handler(event){
-	//console.log(event);
-	//console.log("clicked!");
 	var selected = document.getElementById("selectedButton");
-	//console.log(selected.id);
 	var target = event.target;
-	//console.log("here1");
     var clickedEle = event.target.id ;
-	//console.log(clickedEle);
     var ele = document.getElementById(clickedEle);
-	//console.log(ele);
-	//console.log(ele.innerHTML);
 	selected.innerHTML = "Selected: " + ele.innerHTML;
-	//console.log("here2");
 }	
